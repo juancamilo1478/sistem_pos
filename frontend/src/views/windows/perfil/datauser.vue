57e2a5b5d219f874462cdebb6294a304d9fe3ef2
juan camilo <118995594+juancamilo1478@users.noreply.github.com>
2024-02-15 01:36:57 -0500

    save

--- frontend/src/views/windows/perfil/datauser.vue
+++ frontend/src/views/windows/perfil/datauser.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>darauser</h1>
-  </template>
+  <h1>ponga los datos de user</h1>
+</template>
+<script>
+import axios from "../../../axiosInstance";
+import { toast } from "vue3-toastify";
+export default {
+  data() {
+    return {
+      
+      userdata: [],
+      
+    }
+  },
+  methods: {
+   async loaduser(){
+    const response = await axios.get('users/data')
+    console.log(response.data)
+    }
+  },
+  async mounted() {
+   
+  this.loaduser();
+  },
+};
+</script>
