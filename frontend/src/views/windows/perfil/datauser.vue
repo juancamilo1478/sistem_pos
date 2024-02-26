f3b5693ba4c90f37bba481e94c3db1e5c431f563
juan camilo <118995594+juancamilo1478@users.noreply.github.com>
2024-02-26 13:10:05 -0500

    save

--- frontend/src/views/windows/perfil/datauser.vue
+++ frontend/src/views/windows/perfil/datauser.vue
@@ -1,27 +1,38 @@
 <template>
-  <h1>ponga los datos de user</h1>
+  <div v-if="Object.keys(userdata).length !== 0">
+    <ul class="list-group">
+      <li class="list-group-item">Nombre: {{ userdata.name }}</li>
+      <li class="list-group-item">Correo: {{ userdata.gmail }}</li>
+      <li class="list-group-item">Rol: {{ userdata.category }}</li>
+    
+    </ul>
+  </div>
 </template>
+
 <script>
 import axios from "../../../axiosInstance";
 import { toast } from "vue3-toastify";
+
 export default {
   data() {
     return {
-      
-      userdata: [],
-      
+      userdata: {}
     }
   },
   methods: {
-   async loaduser(){
-    const response = await axios.get('users/data')
-    this.userdata=response.data.data
- 
+    async loaduser() {
+      try {
+        const response = await axios.get('users/data');
+        this.userdata = response.data.data;
+        console.log(this.userdata);
+      } catch (error) {
+        console.error("Error while loading user data:", error);
+        toast.error("Error while loading user data");
+      }
     }
   },
   async mounted() {
-   
-  this.loaduser();
+    this.loaduser();
   },
 };
 </script>
